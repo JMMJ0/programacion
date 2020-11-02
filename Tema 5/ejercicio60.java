f7c90a7b52eb1ea6c563562eb1307cee535d28a1
JMMJ0 <josemanuel.moron.alu@iescampanillas.com>
2020-11-02 11:58:31 +0100

    act

---
+++ Tema 5/ejercicio60.java
@@ -0,0 +1,19 @@
+/*
+Hecho por Jose Manuel Moron Jurado
+*/
+ public class ejercicio60 {
+  public static void main(String[] args) {
+    int altura;
+    
+    
+    System.out.print("Introduzca la altura");
+    altura =Integer.parseInt(System.console().readLine());
+    
+    for(int i =1; i<=altura; i++){
+      System.out.println("***   ***   ");
+    }
+    for(int j= 1; j<=2; j++){
+      System.out.println("***** *****");
+    }
+  }
+}
