b58b5bd168f6d5c2e5551883e1c03e38c11f253b
JMMJ0 <josemanuel.moron.alu@iescampanillas.com>
2020-11-09 09:39:47 +0100

    Tema 6

---
+++ Tema 6/ejercicio01.java
@@ -0,0 +1,12 @@
+/*
+Hecho por José Manuel Morón Jurado
+*/
+public class ejercicio01 {
+  public static void main(String[] args) {
+    
+    for(int i=1; i<=3; i++){
+      int dado=(int)(Math.random()*6);
+      System.out.println("Tirada= "+dado);
+    }
+  }
+}
